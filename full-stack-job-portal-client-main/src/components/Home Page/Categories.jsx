d9770b73f8c4fd70d4c308f7cdc263bc4e5fb14a
tejas527 <tejasdumbhare@gmail.com>
2025-07-03 15:02:12 +0530

    changed categories

--- full-stack-job-portal-client-main/src/components/Home Page/Categories.jsx
+++ full-stack-job-portal-client-main/src/components/Home Page/Categories.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import { MdOutlineCurrencyRupee } from "react-icons/md";
-import { GrCertificate } from "react-icons/gr";
-import { FiPackage } from "react-icons/fi";
 
 const category = [
   { id: 1, cat: "Remote", count: "21.4K+ jobs",icon:<ion-icon name="home-outline"></ion-icon> },
-  { id: 2, cat: "MNC", count: "84K+ jobs",icon:<ion-icon name="business-outline"></ion-icon> },
-  { id: 3, cat: "Sales", count: "22K+ jobs",icon:<ion-icon name="briefcase-outline"></ion-icon> },
-  { id: 4, cat: "Marketing", count: "4.5K+ jobs",icon:<ion-icon name="trending-up-outline"></ion-icon> },
-  { id: 5, cat: "Fortune 500", count: "239 jobs",icon: <ion-icon name="ribbon-outline"></ion-icon>},
-  { id: 6, cat: "Startup", count: "19.7K+ jobs",icon:<ion-icon name="rocket-outline"></ion-icon> },
-  { id: 7, cat: "Project Mgmt", count: "2.2K+ jobs",icon:<ion-icon name="checkmark-circle-outline"></ion-icon> },
-  { id: 8, cat: "Analytics", count: "9.6K+ jobs",icon:<ion-icon name="analytics-outline"></ion-icon> },
-  { id: 9, cat: "HR", count: "4.4K+ jobs",icon:<ion-icon name="people-outline"></ion-icon>},
-  { id: 10, cat: "Engineering", count: "7.8K+ jobs",icon:<ion-icon name="settings-outline"></ion-icon> },
-  { id: 11, cat: "Fresher", count: "70.4K+ jobs",icon:<ion-icon name="school-outline"></ion-icon> },
-  { id: 12, cat: "Data Science", count: "3.9K+ jobs",icon:<ion-icon name="stats-chart-outline"></ion-icon> },
-  { id: 13, cat: "Supply Chain", count: "2.6K+ jobs",icon:<FiPackage/> },
-  { id: 14, cat: "Software & IT", count: "37.7K+ jobs",icon:<ion-icon name="laptop-outline"></ion-icon> },
-  { id: 15, cat: "Banking & Finance", count: "7.6K+ jobs",icon:<MdOutlineCurrencyRupee/> },
-  { id: 16, cat: "Internship", count: "6K+ jobs",icon:<GrCertificate/> },
+  { id: 2, cat: "Machine Learning", count: "84K+ jobs",icon:<ion-icon name="hardware-chip-outline"></ion-icon> },
+  { id: 3, cat: "MERN Stack Developer", count: "22K+ jobs",icon: <ion-icon name="code-slash-outline"></ion-icon> },
+  { id: 4, cat: "Backend Developer", count: "4.5K+ jobs",icon:<ion-icon name="git-branch-outline"></ion-icon> },
+  { id: 5, cat: "UI UX Designer", count: "239 jobs",icon: <ion-icon name="easel-outline"></ion-icon>},
+  { id: 6, cat: "Marketing", count: "19.7K+ jobs",icon:<ion-icon name="briefcase-outline"></ion-icon> },
+  { id: 7, cat: "AI", count: "2.2K+ jobs",icon:<ion-icon name="terminal-outline"></ion-icon> },
+  { id: 8, cat: "Engineering", count: "7.8K+ jobs",icon:<ion-icon name="settings-outline"></ion-icon> },
+  { id: 9, cat: "Data Science", count: "3.9K+ jobs",icon:<ion-icon name="stats-chart-outline"></ion-icon> },
+  { id: 10, cat: "Software & IT", count: "37.7K+ jobs",icon:<ion-icon name="laptop-outline"></ion-icon> },
 ];
 
 const Categories = () => {
@@ -29,8 +20,8 @@ const Categories = () => {
   const shuffled = [...rest].sort(() => 0.5 - Math.random()).slice(0, 9);
 
   const mixedCards = [...fixed, ...shuffled]; 
-  const firstRow = mixedCards.slice(0, 6);
-  const secondRow = mixedCards.slice(6, 11);
+  const firstRow = mixedCards.slice(0, 5);
+  const secondRow = mixedCards.slice(5, 10);
 
   const colors = ["bg-blue-500/10", "bg-purple-500/10", "bg-orange-500/10", "bg-yellow-400/10"];
 
@@ -38,7 +29,7 @@ const Categories = () => {
     <div className="max-w-7xl mx-auto md:px-12 px-6 mb-20">
       <h2 className="md:text-3xl text-2xl font-semibold mb-5">Trending On HireNext</h2>
         <div className="hidden md:block">
-            <div className="grid grid-cols-6 gap-4 mb-4">
+            <div className="grid grid-cols-5 gap-4 mb-4">
                 {firstRow.map(({ cat, id, icon }, i) => (
                     <div
                     key={id}
@@ -57,7 +48,7 @@ const Categories = () => {
                 ))}
             </div>
 
-            <div className="grid grid-cols-5 gap-4 px-20">
+            <div className="grid grid-cols-5 gap-4 px-10">
                 {secondRow.map(({ cat, id, icon }, i) => (
                     <div
                     key={id}
